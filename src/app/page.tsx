a6b758addbb8f6bbdbf5a10bf3a46cc89cac1312
Daffa <daffamfzn@gmail.com>
2023-11-29 12:11:12 +0700

    Added responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center bg-[#F6F4F1] border-8 border-black">
+    <main className="min-h-screen flex flex-col justify-center items-center bg-[#F6F4F1] border-4 md:border-8 border-black">
       <div className="flex flex-col justify-center items-center">
-        <span className="text-[50px] font-medium tracking-widest">
+        <span className="text-xl lg:text-[50px] font-medium tracking-widest">
           Fahd Muhammad Zahid
         </span>
         <span className="font-light">a student</span>
