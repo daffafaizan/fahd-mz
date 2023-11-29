c1e55733d88d6189dc38171dd6c8b489fda1fee9
Daffa <daffamfzn@gmail.com>
2023-11-29 11:38:26 +0700

    Basic website made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main className="">
-
+    <main className="min-h-screen flex flex-col justify-center items-center bg-[#F6F4F1] border-8 border-black">
+      <div className="flex flex-col justify-center items-center">
+        <span className="text-[50px] font-medium tracking-widest">
+          Fahd Muhammad Zahid
+        </span>
+        <span className="font-light">a student</span>
+      </div>
     </main>
-  )
+  );
 }
